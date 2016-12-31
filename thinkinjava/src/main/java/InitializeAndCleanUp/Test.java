2aaadad6b1c2d2146e46f245d6b9ff8dc9c45e52
sean417 <373202226@qq.com>
2016-12-31 11:21:46 +0800

    update code

---
+++ thinkinjava/src/main/java/InitializeAndCleanUp/Test.java
@@ -0,0 +1,7 @@
+package InitializeAndCleanUp;
+
+/**
+ * Created by chenyang on 2016/12/31.
+ */
+public class Test {
+}
---
+++ thinkinjava/src/main/java/InitializeAndCleanUp/part5/A.java
@@ -0,0 +1,7 @@
+package InitializeAndCleanUp.part5;
+
+/**
+ * Created by chenyang on 2016/12/31.
+ */
+public class A {
+}

425a2661d6c4f98790ffb35f9df1a2490e6ee73d
sean417 <373202226@qq.com>
2017-01-17 19:57:18 +0800

    update code

--- thinkinjava/src/main/java/InitializeAndCleanUp/part7/OrderOfInitialization.java
+++ thinkinjava/src/main/java/InitializeAndCleanUp/part7/OrderOfInitialization.java
@@ -1,7 +1,34 @@
 package InitializeAndCleanUp.part7;
 
-/**
+import static net.mindview.util.Print.print;
+
+/**初始化顺序：
+ * 1.变量定义的鲜花顺序决定了初始化的顺序。
+ * 2.即使变量定义散布于方法定义之间，他们仍然会在任何方法（包括构造器）被调用之前
  * Created by chenyang on 2017/1/17.
  */
+
+class Window{
+    Window(int marker){
+        print("Window("+marker+")");
+    }
+}
+
+class House{
+    Window w1=new Window(1);
+    House(){
+        print("House()");
+        w3=new Window(33);
+    }
+    Window w2=new Window(2);
+    void f(){print("f()");}
+    Window w3=new Window(3);
+
+}
+
 public class OrderOfInitialization {
+    public static void main(String[] args) {
+        House h=new House();
+        h.f();
+    }
 }
---
+++ thinkinjava/src/main/java/InitializeAndCleanUp/part7/StaticInitialization.java
@@ -0,0 +1,23 @@
+package InitializeAndCleanUp.part7;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Created by chenyang on 2017/1/17.
+ */
+class Bowl{
+    Bowl(int marker){
+        print("Bowl("+marker+")");
+    }
+    void f1(int marker){
+        print("f1("+marker+")");
+    }
+}
+
+class Table{
+
+}
+
+public class StaticInitialization {
+
+}

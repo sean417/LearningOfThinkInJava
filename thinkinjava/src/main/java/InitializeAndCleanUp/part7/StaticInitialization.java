836cd3b1265095f5ca8117dabd81502a60c6a8b0
sean417 <373202226@qq.com>
2017-01-18 09:33:32 +0800

    update code

--- thinkinjava/src/main/java/InitializeAndCleanUp/part7/StaticInitialization.java
+++ thinkinjava/src/main/java/InitializeAndCleanUp/part7/StaticInitialization.java
@@ -1,5 +1,6 @@
 package InitializeAndCleanUp.part7;
 
+import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
 import static net.mindview.util.Print.print;
 
 /**
@@ -15,9 +16,37 @@ class Bowl{
 }
 
 class Table{
+    static Bowl bowl1=new Bowl(1);
+    Table(){
+        print("Table()");
+        bowl2.f1(1);
+    }
+    void f2(int marker){
+        print("f2("+marker+")");
+    }
+    static Bowl bowl2=new Bowl(2);
+}
 
+class Cupboard{
+    Bowl bowl3=new Bowl(3);
+    static Bowl bowl4=new Bowl(4);
+    Cupboard(){
+        print("Cupboard");
+        bowl4.f1(2);
+    }
+    void f3(int marker){
+        print("f3("+marker+")");
+    }
+    static Bowl bowl5=new Bowl(5);
 }
 
 public class StaticInitialization {
-
+    public static void main(String[] args) {
+        print("Creating new Cupboard() in main");
+        new Cupboard();
+        print("Creating new Cupboard() in main");
+        new Cupboard();
+    }
+    static Table table=new Table();
+    static Cupboard cupboard=new Cupboard();
 }

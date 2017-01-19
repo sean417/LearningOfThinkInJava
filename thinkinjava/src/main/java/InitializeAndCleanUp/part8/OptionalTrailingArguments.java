72a67c6dd4a1ff99488bbc9952f868a0815c3034
sean417 <373202226@qq.com>
2017-01-19 13:59:27 +0800

    update code

--- thinkinjava/src/main/java/InitializeAndCleanUp/part8/OptionalTrailingArguments.java
+++ thinkinjava/src/main/java/InitializeAndCleanUp/part8/OptionalTrailingArguments.java
@@ -1,11 +1,23 @@
 package InitializeAndCleanUp.part8;
 
 /**
+ * 可变参数可以是0个参数
+ * 当具有可选的尾随参数时，这个特性很有用。
  * Created by chenyang on 2017/1/19.
  */
 public class OptionalTrailingArguments {
     static void f(int required,String... trailing)
     {
         System.out.println("required:"+required+" ");
+        for(String s:trailing){
+            System.out.println(s+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        f(1,"one");
+        f(2,"two","three");
+        f(0);
     }
 }

2fcda415adf728cd5dcb32cb56d5ac0dbf8964ae
sean417 <373202226@qq.com>
2017-02-12 13:09:11 +0800

    将一个通道和另一个通道直接相连

--- thinkinjava/src/main/java/io/part10/TransferTo.java
+++ thinkinjava/src/main/java/io/part10/TransferTo.java
@@ -2,18 +2,25 @@ package io.part10;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
+ *
+ * 特殊方法transferTo和transferFrom()允许我们将一个通道和另一个通道直接相连：
  * Created by chenyang on 2016/10/21.
  */
 public class TransferTo {
 
     public static void main(String[] args)throws Exception {
+        URL urlSrc = GetChannel.class.getResource("/data.txt" );
+
+        URL urlDes = GetChannel.class.getResource("/data1.txt" );
+
         FileChannel
-                in=new FileInputStream("./src/main/java/io/source/data1.txt").getChannel(),
-                out=new FileOutputStream("./src/main/java/io/source/data2.txt").getChannel();
+                in=new FileInputStream(urlSrc.getFile()).getChannel(),
+                out=new FileOutputStream(urlDes.getFile()).getChannel();
         in.transferTo(0,in.size(),out);
     }
 }

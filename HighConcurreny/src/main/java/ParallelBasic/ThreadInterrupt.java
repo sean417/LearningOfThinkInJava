3b3deaec7707ede9b7cf731300863ba92c3282ac
sean417 <373202226@qq.com>
2017-02-22 18:25:22 +0800

    主线程中断子线程

--- HighConcurreny/src/main/java/ParallelBasic/ThreadInterrupt.java
+++ HighConcurreny/src/main/java/ParallelBasic/ThreadInterrupt.java
@@ -1,6 +1,7 @@
 package ParallelBasic;
 
-/**主线程中断子线程：
+/**中断案例1：
+ * 主线程中断子线程：
  * 主线程操作子线程t1.interrupt();然后子线程检查是否被中断的状态Thread.currentThread().interrupted()
  * 当为true时，break;
  * Created by chenyang on 2017/2/22.

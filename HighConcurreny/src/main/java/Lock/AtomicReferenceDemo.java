7397fcf2a58873d4d32e018068095fb65381993c
sean417 <373202226@qq.com>
2017-02-28 12:09:09 +0800

    使用引用类型AtomicReference<T>的原子操作。

---
+++ HighConcurreny/src/main/java/Lock/AtomicReferenceDemo.java
@@ -0,0 +1,35 @@
+package Lock;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**使用引用类型AtomicReference<T>的原子操作。
+ *
+ *
+ * Created by chenyang on 2017/2/28.
+ */
+public class AtomicReferenceDemo {
+   public static final AtomicReference<String> atomicStr=new AtomicReference<String>("abc");
+
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            final int num=i;
+            new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(Math.abs((int)(Math.random()*100)));
+                    }catch (InterruptedException e){
+                        e.printStackTrace();;
+                    }
+                    if(atomicStr.compareAndSet("abc","def")) {
+                        System.out.println("Thread"+Thread.currentThread().getId()+" changed");
+                    }
+                    else {
+                        System.out.println("Thread"+Thread.currentThread().getId()+" fail");
+                    }
+                }
+            }.start();
+        }
+    }
+
+}

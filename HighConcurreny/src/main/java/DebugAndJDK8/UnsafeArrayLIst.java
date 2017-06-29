0fa5b6c9099b563bb2abbd1c0513c4ed6cd9679c
sean417 <373202226@qq.com>
2017-06-29 23:37:15 +0800

    update code

--- HighConcurreny/src/main/java/DebugAndJDK8/UnsafeArrayLIst.java
+++ HighConcurreny/src/main/java/DebugAndJDK8/UnsafeArrayLIst.java
@@ -1,8 +1,41 @@
 package DebugAndJDK8;
 
+import java.util.ArrayList;
+
 /**
  * Created by chenyang on 2017/6/29.
  */
 public class UnsafeArrayLIst {
+   static ArrayList al=new ArrayList();
+   static class AddTask implements Runnable{
+       public void run() {
+           try{
+               Thread.sleep(100);
+           }catch (InterruptedException e){
 
+           }
+           for(int i=0;i<1000000;i++){
+               al.add(new Object());
+           }
+       }
+   }
+
+    public static void main(String[] args) throws InterruptedException{
+        Thread t1=new Thread(new AddTask(),"t1");
+        Thread t2=new Thread(new AddTask(),"t2");
+        t1.start();
+        t2.start();
+        Thread t3=new Thread(new Runnable() {
+            public void run() {
+                while (true){
+                    try{
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){
+
+                    }
+                }
+            }
+        },"t3");
+        t3.start();
+    }
 }

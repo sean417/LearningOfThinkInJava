cc0fdc5acb6a032602a06dd2935f10999290c421
sean417 <373202226@qq.com>
2017-07-31 23:04:24 +0800

    StampedLock是一个乐观读写锁。

---
+++ HighConcurreny/src/main/java/concurrentDebug/LongAdderTest.java
@@ -0,0 +1,11 @@
+package concurrentDebug;
+
+/**
+ *
+ * Created by chenyang3@01zhuanche.com on 2017/7/30.
+ */
+public class LongAdderTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ HighConcurreny/src/main/java/concurrentDebug/StampedLockTest.java
@@ -0,0 +1,44 @@
+package concurrentDebug;
+
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * StampedLock是一个乐观读写锁。
+ * 通过使用tryOptimisticRead()或tryOptimisticWrite()来实现读和写线程可以同时
+ * 操作数据，同时用stamp来维护版本，使数据不会发生不一致。
+ * Created by chenyang3@01zhuanche.com on 2017/7/31.
+ */
+public class StampedLockTest {
+    private double x,y;
+    private  final StampedLock sl=new StampedLock();
+
+    void move(double deltaX,double deltaY){
+        long stamp=sl.writeLock();
+        try{
+            x+=deltaX;
+            y+=deltaY;
+        }finally {
+            sl.unlockWrite(stamp);
+        }
+    }
+
+    double distanceFromOrigin(){
+    long stamp=sl.tryOptimisticRead();
+    double currentX=x,currentY=y;
+    if(!sl.validate(stamp)){
+        stamp=sl.readLock();
+        try{
+            currentX=x;
+            currentY=y;
+        }finally {
+            sl.unlockRead(stamp);
+        }
+    }
+    return Math.sqrt(currentX*currentX+currentY*currentY);
+    }
+
+    public static void main(String[] args) {
+        StampedLockTest test=new StampedLockTest();
+        test.distanceFromOrigin();
+    }
+}

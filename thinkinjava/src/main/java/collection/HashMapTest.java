304810a0ebebb4b08e3c1aa8329ef09817098b8f
sean417 <373202226@qq.com>
2017-08-21 11:28:18 +0800

    StampedLock是一个乐观读写锁。

---
+++ thinkinjava/src/main/java/collection/HashMapTest.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<Integer,String> hash=new HashMap<Integer,String>();
+        hash.put(1,"aa");
+        hash.put(17,"aa");
+        hash.put(33,"aa");
+        hash.put(2,"bb");
+        hash.put(3,"cc");
+        hash.put(4,"dd");
+
+    }
+}

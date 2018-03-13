e29ecd48703e81edecba5bad6ee08e46baee2b32
sean417 <373202226@qq.com>
2018-03-13 09:05:27 +0800

    栈上分配

--- jvminaction/src/main/java/jvmbasicstructure/OnStackTest.java
+++ jvminaction/src/main/java/jvmbasicstructure/OnStackTest.java
@@ -3,7 +3,7 @@ package jvmbasicstructure;
 /**
  *
  * 第一种运行方式：-server -Xmx10m -Xms10m -XX:+DoEscapeAnalysis -XX:+PrintGC
- * 这种方式new对象在栈上分配，gc不参与
+ * 这种方式new对象在栈上分配，gc不参与回收，因为变量仅仅在栈上分配空间，降低gc的工作量，同时防止堆上的空间被占用
  * 第二种运行方式：-server -Xmx10m -Xms10m -XX:-DoEscapeAnalysis -XX:+PrintGC
     这种方式new对象在java堆上分配，gc参与释放
  * Created by chenyang on 2017/1/25.

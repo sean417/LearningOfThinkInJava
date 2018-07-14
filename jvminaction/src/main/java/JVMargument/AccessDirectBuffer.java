f36c32011a4892417027e72fe4c679ca45721d54
sean417 <373202226@qq.com>
2018-07-14 23:35:40 +0800

    直接内存配置

--- jvminaction/src/main/java/JVMargument/AccessDirectBuffer.java
+++ jvminaction/src/main/java/JVMargument/AccessDirectBuffer.java
@@ -1,5 +1,7 @@
 package JVMargument;
 
+import java.nio.ByteBuffer;
+
 /**
  *
  * 直接内存配置：
@@ -10,7 +12,18 @@ package JVMargument;
  */
 public class AccessDirectBuffer {
     public void directAccess(){
-
+        long starttime=System.currentTimeMillis();
+        ByteBuffer b=ByteBuffer.allocateDirect(500);
+        for(int i=0;i<100000;i++){
+            for(int j=0;i<99;j++){
+                b.putInt(j);
+            }
+            b.flip();
+            for(int j=0;j<99;j++){
+                b.getInt();
+            }
+            b.clear();
+        }
     }
 
 
